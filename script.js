ff074fbf32fb77f4987ab4d0571997677754d153
Prafull Garg <gargprafull55@gmail.com>
2023-11-18 13:20:11 +0530

    feat: Add effects to grid items on hover
    I have added event listner to all the grid boxes which will change background color of the box when listens to mouseover event.

--- script.js
+++ script.js
@@ -4,15 +4,15 @@ const  gridContainer = document.createElement("div")
 gridContainer.classList.add("grid")
 body.appendChild(gridContainer)
 
-for(let i=0 ; i<16 ;i++)
-{
+function createGrid() {
+    for(let i=0 ; i<16 ;i++){
     for(let j=0 ;j<16 ; j++){
         const gridItem = document.createElement("div")
-        
-        gridItem.classList.add("grid-item")
-        gridItem.innerHTML ="";
-        
+        gridItem.classList.add("grid-item")        
         gridContainer.appendChild(gridItem)
-    }
-}
+        gridItem.addEventListener('mouseover',(event)=>{
+            event.target.classList.add('hover')
+        })
+    }}}
 
+createGrid()
