8dc7336bc085babfa74b17520c7cb2868a063c57
Prafull Garg <gargprafull55@gmail.com>
2023-11-18 14:20:06 +0530

    feat: Add button to change grid size
    Added button which which ask for grid size from user through promt and widthout changing contaier size will form new grid with provided.

--- script.js
+++ script.js
@@ -1,18 +1,41 @@
 const body = document.querySelector("body")
+const gridSizeButton =  document.createElement('button')
 const  gridContainer = document.createElement("div")
 
+gridSizeButton.textContent= "Change Grid Size"
 gridContainer.classList.add("grid")
+
+body.appendChild(gridSizeButton)
 body.appendChild(gridContainer)
 
-function createGrid() {
-    for(let i=0 ; i<16 ;i++){
-    for(let j=0 ;j<16 ; j++){
+function createGrid(size =16) {
+    console.log(size)
+    for(let i=0 ; i<size ;i++){
+    for(let j=0 ;j<size ; j++){
         const gridItem = document.createElement("div")
-        gridItem.classList.add("grid-item")        
+        gridItem.style.border='1px solid black';
+        gridItem.style.boxSizing='border-box';
+        gridItem.style.width = `${768/size}px`;
+        gridItem.style.height= `${768/size}px`;
+
         gridContainer.appendChild(gridItem)
         gridItem.addEventListener('mouseover',(event)=>{
             event.target.classList.add('hover')
         })
     }}}
+
+function askGridSize(){
+    return prompt("Enter Grid Size")
+
+}
+
+function changeGrid(){
+    gridContainer.innerHTML='';
+    let size = askGridSize();
+    createGrid(size);
+}
+
+gridSizeButton.addEventListener('click',changeGrid);
+
 
 createGrid()
